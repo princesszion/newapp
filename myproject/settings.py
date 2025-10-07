f7bbf8964c7dc2fd404251bd19610ba26cff4c1c
princesszion <maikemvictorine@gmail.com>
2025-10-07 18:37:20 -0400

    made changes

--- myproject/settings.py
+++ myproject/settings.py
@@ -170,7 +170,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = "@r*-t*njmi-vd&m5dcm0&42a%6lek08a*_74z)i!1)vxrk^9rs"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -226,15 +226,7 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-#DATABASES = {
-#    "default": {
-#        "ENGINE": 'django.db.backends.postgresql',
-#        "NAME": env('DB_NAME'),
-#        "HOST": env('DB_HOST'),
-#        "USER": env('DB_USER'),
-#        "PASSWORD": env('DB_PASSWORD'),
-#    }
-#}
+
 
 DATABASES = {
      "default": {
