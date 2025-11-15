a6a6ab0dc1a690efafa349d7e1f8006502cb236d
princesszion <maikemvictorine@gmail.com>
2025-11-15 13:24:34 -0500

    Added all changes except settings.py

--- myblog/admin.py
+++ myblog/admin.py
@@ -1,6 +1,33 @@
+# from django.contrib import admin
+
+# from .models import BlogPost,Contact
+
+# admin.site.register(BlogPost)
+# admin.site.register(Contact)
+
+
 from django.contrib import admin
+from django import forms
+from ckeditor.widgets import CKEditorWidget
 
-from .models import BlogPost,Contact
+from .models import BlogPost, Contact
 
-admin.site.register(BlogPost)
-admin.site.register(Contact)
+
+class BlogPostAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorWidget())
+
+    class Meta:
+        model = BlogPost
+        fields = '__all__'
+
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    form = BlogPostAdminForm
+    list_display = ('title', 'author', 'created_at')
+    search_fields = ('title', 'author__username', 'author__first_name', 'author__last_name')
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'email', 'subject', 'organisation')
--- myblog/models.py
+++ myblog/models.py
@@ -1,16 +1,29 @@
 from django.db import models
+from django.conf import settings   # <– import this
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    author = models.CharField(max_length=100)
+
+    # CHANGE THIS:
+    # author = models.CharField(max_length=100)
+
+    # TO THIS:
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,          # usually auth.User
+        on_delete=models.CASCADE,         # Deletes posts if user is deleted
+        null=True,                         # allow temporary null during migration
+        blank=True,
+        related_name='blog_posts'
+    )
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
 
     def __str__(self):
         return self.title
-    
+
 
 class Contact(models.Model):
     full_name = models.CharField(max_length=100)
